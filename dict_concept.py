71ca5f0bdf0267f92e208942cebf4842c0117822
Erick Fernando Holguin Ceron <subtanicdos3@gmail.com>
2026-03-28 14:48:19 -0500

    2 func

--- dict_concept.py
+++ dict_concept.py
@@ -1,1 +1,6 @@
 # Write your code here!
+def temp_and_color(data):
+    
+    temp_value = data.get("temp", None)
+    color_value = data.get("color", None)
+    return (temp_value, color_value)
--- dict_func.py
+++ dict_func.py
@@ -1,6 +1,22 @@
 # Write your code here!
-def get_config():
-    return{"width": 480, 
-           "height": 480,
-           "color_mode": "dark",
-           "sensitivity": 0.4}
+def employee_print(employee_info):
+
+    name = employee_info.get("Name", "N/A")
+    salary = employee_info.get("Salary", "N/A")
+    role = employee_info.get("Role", "N/A")
+
+    print(f"Name: {name}")
+    print(f"Salary: {salary}")
+    print(f"Role: {role}")
+    
+    #reabre el diccionario con nuevos datos sin afectar los originales
+    remaining_info = dict(employee_info)
+    remaining_info.pop("Name", None)
+    remaining_info.pop("Salary", None)
+    remaining_info.pop("Role", None)
+
+    if remaining_info:
+        for key, value in remaining_info.items():
+            print(f"{key}: {value}")
+    else:
+        print("No other info!")
