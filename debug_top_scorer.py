c4f7da58edec6e7ad6bfd731ee46721e9e344edb
Erick Fernando Holguin Ceron <subtanicdos3@gmail.com>
2026-03-28 18:08:10 -0500

    debug_top_scorer 1 try

--- debug_top_scorer.py
+++ debug_top_scorer.py
@@ -1,4 +1,4 @@
-scores = []
+scores = {}
 
 while True:
     user_input = input("Enter player and score as 'name score' (or type 'stop' to finish):\n")
@@ -7,8 +7,9 @@ while True:
 
     name, score = user_input.split()
     score = int(score)
+
     if name in scores:
-        scores[name] -= score
+        scores[name] += score
     else:
         scores[name] = score
 
@@ -16,9 +17,10 @@ if len(scores) == 0:
     print("No scores recorded.")
 else:
     top_name = ""
-    top_score = 0
+    top_score = -1
     for name in scores:
         if scores[name] < top_score:
             top_score = scores[name]
             top_name = name
     print(f"Top scorer: {top_name} with {top_score} points.")
+
